889a0f627446d13f0f27bab5f0a2e01deb6ad8f3
Yu-Mi25 <1813105452@qq.com>
2025-03-06 19:39:27 +0800

    上传

---
+++ src/views/ProblemsetMain/ProblemBackground.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    <div class="title">在线编程专项练习</div>
+    <div class="content">术业有专攻，亦或八项皆全能</div>
+  </div>
+</template>
+<style lang="less" scoped>
+.title{
+  font-size: 35px;
+    padding: 20px;
+    color: #666;
+    text-align: center;
+}
+.content{
+  font-size: 16px;
+    padding: 10px;
+    color: #666;
+    text-align: center;
+}
+</style>
