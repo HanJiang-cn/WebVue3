e0e3419c815ba360dbf694a9b2654be4c5a49666
Yu-Mi25 <1813105452@qq.com>
2025-03-15 16:46:27 +0800

    上传

---
+++ src/layouts/StoreLayout.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import navMenu from '@/components/navMenu/navMenu.vue'
+import StoreMain from '@/views/StoreMain/StoreMain.vue';
+</script>
+<template>
+    <div class="default-layout">
+    <div class="header">
+      <div class="nav-menu">
+        <navMenu />
+      </div>
+    </div>
+    <div>
+      <StoreMain />
+    </div>
+  </div>
+</template>
+<style lang="less" scoped>
+.header {
+  height: 50px;
+  border-bottom: #EBEBEB 1px solid;
+  overflow: hidden;
+
+  .nav-menu {
+    max-width: 1700px;
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+  }
+}
+</style>
